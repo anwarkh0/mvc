d6056a413a158ffbb2b24313800c5bdd20e4bcbf
anwar khawli <anwarkhawle@gmail.com>
2023-12-13 01:36:22 +0200

    user model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+'use strict';
+import { Model } from "sequelize";
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      // define association here
+      User.hasMany(models.TransactionModel,{
+        foreignKey:"userid",
+        onUpdate:"CASCADE",
+        onDelete:"CASCADE"
+      })
+    }
+  }
+  User.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
